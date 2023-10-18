69cabb2ed63035502900e9d98d921d4a8191e65f
Bharath <106143429+bharathajjarapu@users.noreply.github.com>
2023-10-18 21:31:29 +0530

    Main

---
+++ app.py
@@ -0,0 +1,60 @@
+from flask import Flask, render_template, request
+import cv2
+import numpy as np
+
+app = Flask(__name__)
+
+net = cv2.dnn.readNet('tiny.weights', 'tiny.cfg')
+classes = []
+with open('classes.txt', 'r') as f:
+    classes = [line.strip() for line in f.readlines()]
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    img = request.files['image']
+    img.save('static/input.jpg')
+
+    image = cv2.imread('static/input.jpg')
+    height, width, _ = image.shape
+    blob = cv2.dnn.blobFromImage(image, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
+    net.setInput(blob)
+    outs = net.forward(net.getUnconnectedOutLayersNames())
+    class_ids = []
+    confidences = []
+    boxes = []
+    for out in outs:
+        for detection in out:
+            scores = detection[5:]
+            class_id = np.argmax(scores)
+            confidence = scores[class_id]
+            if confidence > 0.5:
+                center_x = int(detection[0] * width)
+                center_y = int(detection[1] * height)
+                w = int(detection[2] * width)
+                h = int(detection[3] * height)
+                x = int(center_x - w / 2)
+                y = int(center_y - h / 2)
+                boxes.append([x, y, w, h])
+                confidences.append(float(confidence))
+                class_ids.append(class_id)
+
+    indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
+    font = cv2.FONT_HERSHEY_PLAIN
+    colors = np.random.uniform(0, 255, size=(len(classes), 3))
+    for i in range(len(boxes)):
+        if i in indexes:
+            x, y, w, h = boxes[i]
+            label = str(classes[class_ids[i]])
+            color = colors[class_ids[i]]
+            cv2.rectangle(image, (x, y), (x + w, y + h), color, 2)
+            cv2.putText(image, label, (x, y + 30), font, 1, color, 2)
+
+    cv2.imwrite('static/output.jpg', image)
+    return render_template('result.html', input_image='static/input.jpg', output_image='static/output.jpg')
+
+if __name__ == '__main__':
+    app.run(debug=True)
